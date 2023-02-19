c6502b2bcb501b6a8d227957a71450e6803fe7d5
Alejandro jose Ruzafa casas <aruzafa-@c3r8s4.42malaga.com>
2023-02-19 21:10:38 +0100

    pipex

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:30:41 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/02/19 17:42:50 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/02/19 18:58:33 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PIPEX_H
 
 # include "unistd.h"
+# include "fcntl.h"
 # include "libft/ft.h"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,37 +6,108 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:01:53 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/02/19 18:30:37 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/02/19 21:09:36 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+char	*concat_slash(char *c)
 {
-	int		pid;
-	char	**command;
-	
-	pid = fork();
-	(void) argc;
-	if (pid == 0) // fillo
+	char	*res;
+	size_t	len;
+
+	len = ft_strlen(c);
+	res = (char *) ft_calloc(len + 2, sizeof(char));
+	ft_memcpy(res, c, len);
+	res[len + 1] = '/';
+	return (res);
+}
+
+char	**get_path(char **env)
+{
+	char	**arg;
+	char	**res;
+	char	*true_res;
+
+	while (env)
 	{
-		command = ft_split(argv[2], ' ');
-		if (!access(command[0], F_OK | X_OK))
+		arg = ft_split(*env, '=');
+		if (ft_strncmp(arg[0], "PATH", 4) == 0)
 		{
-			// Verified that exists and can be executed
-			execve(command[0], command, 0);
+			free(arg[0]);
+			res = ft_split(arg[1], ':');
+			int i = 0;
+			while (res[i])
+			{
+				true_res = concat_slash(res[i]);
+				free(res[i]);
+				res[i] = true_res;
+				i++;
+			}
+			free(arg[1]);
+			free(arg);
+			return (res);
 		}
 		else
 		{
-			ft_printf("Command does not exists or has not got permission.\n");
+			free(arg[0]);
+			free(arg[1]);
+			free(arg);
 		}
+		env++;
+	}
+	return (0);
+}
+
+void	exec(char **command, char **env)
+{
+	int	res;
+	res = access(command[0], F_OK | X_OK);
+	ft_printf("%d", res);
+	if (res)
+	{
+		// Verified that exists and can be executed
+		execve(command[0], command, env);
+	}
+	else
+	{
+		ft_printf("Command %s does not exists or has not got permission.\n", command[0]);
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	int		pid;
+	char	**command;
+	int		fds[2];
+	char	**path;
+
+	if (argc < 5)
+		return (0);
+	pipe(fds);
+	path = get_path(env);
+	pid = fork();
+	if (pid == 0) // fillo
+	{
+		int fd = open(argv[1], O_RDONLY);
+		dup2(fd, 0);
+		dup2(fds[1], 1);
+		command = ft_split(argv[2], ' ');
+		exec(command, path);
 		exit(0);
 	}
 	else
 	{
 		waitpid(pid, 0, 0);
-		ft_printf("finished waiting for command.\n");
+		int fd2 = open(argv[4], O_WRONLY);
+		dup2(fds[0], 0);
+		dup2(fd2, 1);
+		close(fd2);
+		close(fds[1]);
+		command = ft_split(argv[3], ' ');
+		exec(command, path);
+		ft_printf("\nfinished waiting for command.\n");
 	}
 	return (0);
 }
