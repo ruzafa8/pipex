cc20977694733848eeb6347baff15f6f8dfe2a1d
Alejandro jose Ruzafa casas <aruzafa-@c3r8s4.42malaga.com>
2023-02-19 18:31:29 +0100

    Comprobar que el archivo existe y tiene permisos

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:01:53 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/02/19 18:21:19 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/02/19 18:30:37 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,21 @@ int	main(int argc, char **argv)
 	if (pid == 0) // fillo
 	{
 		command = ft_split(argv[2], ' ');
-		execve(command[0], command, 0);
+		if (!access(command[0], F_OK | X_OK))
+		{
+			// Verified that exists and can be executed
+			execve(command[0], command, 0);
+		}
+		else
+		{
+			ft_printf("Command does not exists or has not got permission.\n");
+		}
 		exit(0);
 	}
 	else
 	{
 		waitpid(pid, 0, 0);
-		ft_printf("finished waiting for command.");
+		ft_printf("finished waiting for command.\n");
 	}
 	return (0);
 }
