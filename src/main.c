193664c43c084cec7f6ff8bf23a8955a0e6c43e9
Alejandro jose Ruzafa casas <aruzafa-@c3r6s5.42malaga.com>
2023-02-27 19:22:50 +0100

    return last command return status

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:01:53 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/02/27 18:22:48 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/02/27 19:19:27 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(int argc, char **argv, char **env)
 	char	**command;
 	int		fds[2];
 	char	**path;
+	int		result_code;
 
 	if (argc < 5)
 		return (103);
@@ -36,7 +37,6 @@ int	main(int argc, char **argv, char **env)
 		close(fds[1]);
 		command = ft_split(argv[2], ' ');
 		px_exec(command, path, env);
-		exit(0);
 	}
 	else
 	{
@@ -57,9 +57,13 @@ int	main(int argc, char **argv, char **env)
 			close(fds[0]);
 			close(fds[1]);
 			waitpid(pid1, 0, 0);
-			waitpid(pid2, 0, 0);
+			waitpid(pid2, &result_code, 0);
+			if (WIFEXITED(result_code))
+			{
+				return (WEXITSTATUS(result_code));
+			}
 		}
 	}
 	px_free_path(path);
-	return (0);
+	return (errno);
 }
