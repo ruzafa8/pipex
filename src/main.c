fb9fb6a61c60ea4c1fd2cbf22cf00114b1506cd8
Alejandro jose Ruzafa casas <aruzafa-@c3r8s4.42malaga.com>
2023-03-11 20:08:08 +0100

    separate pipex function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:01:53 by aruzafa-          #+#    #+#             */
-/*   Updated: 2023/03/11 19:41:35 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2023/03/11 20:07:11 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,40 +40,26 @@ void	second_command(int *fd_pipe, int fd_in, int fd_out, char *command, char **p
 	perror(strerror(res));
 }
 
-int	main(int argc, char **argv, char **env)
+void	pipex(int fd_in, char *command1, char *command2, int fd_out, char **path, char **env)
 {
-	int		pid1;
-	int		pid2;
-	int		fds[2];
-	char	**path;
-	int		result_code;
-	int		fd;
-	int		fd2;
+	int	pid1;
+	int	pid2;
+	int	fds[2];
+	int	result_code;
 
-	if (argc < 5)
-		return (103);
 	pipe(fds);
-	path = px_get_path(env);
 	pid1 = fork();
 	if (pid1 < 0)
-		return (0);
-	if (pid1 == 0) // fillo
-	{
-		fd = open(argv[1], O_RDONLY);
-		first_command(fds, fd, fds[1], argv[2], path, env);
-	}
+		exit(pid1);
+	if (pid1 == 0)
+		first_command(fds, fd_in, fds[1], command1, path, env);
 	else
 	{
 		pid2 = fork();
 		if (pid2 < 0)
-			return (0);
+			exit(pid2);
 		if (pid2 == 0)
-		{
-			fd2 = open(argv[4], O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-			if (fd2 < 0)
-				return (errno);
-			second_command(fds, fds[0], fd2, argv[3], path, env);
-		}
+			second_command(fds, fds[0], fd_out, command2, path, env);
 		else
 		{
 			close(fds[0]);
@@ -81,11 +67,26 @@ int	main(int argc, char **argv, char **env)
 			waitpid(pid1, 0, 0);
 			waitpid(pid2, &result_code, 0);
 			if (WIFEXITED(result_code))
-			{
-				return (WEXITSTATUS(result_code));
-			}
+				exit(WEXITSTATUS(result_code));
 		}
 	}
+
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	char	**path;
+	int		fd;
+	int		fd2;
+
+	if (argc < 5)
+		return (103);
+	path = px_get_path(env);
+	fd = open(argv[1], O_RDONLY);
+	fd2 = open(argv[4], O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd2 < 0)
+		return (errno);
+	pipex(fd, argv[2], argv[3], fd2, path, env);
 	px_free_path(path);
 	return (errno);
 }
